64d08cef866da8ee9118b8d273ad5ecb67a6de44
ducthangho <ducthangho@gmail.com>
2019-10-31 02:32:40 +0700

    - Add setting files

--- a.sh
+++ a.sh
@@ -1,7 +1,6 @@
-WORKING_FOLDER="/mnt/c/Users/mylaptop/Google Drive/3DConverter/3dConverter"
-if [ $# -gt 0 ];
-then
-"$WORKING_FOLDER/out/build/WSL-Release/3dConverter" "$@"
+WORKING_FOLDER="/mnt/c/Users/mylaptop/Google Drive/3DProjects"
+if [ $# -gt 0 ]; then
+"$WORKING_FOLDER/build/3dConverter" "$@"
 else
-"$WORKING_FOLDER/out/build/WSL-Release/3dConverter" "$WORKING_FOLDER/20160125OTC-Conference Center - IFC4.ifc"
+"$WORKING_FOLDER/build/3dConverter" "$WORKING_FOLDER/AC14-FZK-Haus.ifc.ifc"
 fi
